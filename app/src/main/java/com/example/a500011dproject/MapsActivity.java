f51772a2a4e02766db73205dcbca22fd668d4c4c
cnjoanne <scn153@gmail.com>
2023-04-02 22:57:55 +0800

    new branch

--- app/src/main/java/com/example/a500011dproject/MapsActivity.java
+++ app/src/main/java/com/example/a500011dproject/MapsActivity.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
