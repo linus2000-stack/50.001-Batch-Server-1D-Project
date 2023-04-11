be72ba1a4b85a82c48679a63c84430f50fe714aa
Miguel Ong (@ahalation) <agitatehalation@gmail.com>
2023-04-11 20:01:03 +0800

    block

--- app/src/main/java/com/example/a500011dproject/User.java
+++ app/src/main/java/com/example/a500011dproject/User.java
@@ -3,11 +3,16 @@ package com.example.a500011dproject;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.Date;
+import java.util.HashMap;
+
 public class User implements Parcelable {
     private String name;
+    HashMap<Date, String> block;
 
     User(String name) {
         this.name = name;
+        this.block = new HashMap<>();
     }
 
     protected User(Parcel in) {
